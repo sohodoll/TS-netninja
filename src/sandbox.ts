5bb8e646e48e9a4787da806fa24008daec53ba68
sohodoll <kovalevvtl@gmail.com>
2022-12-01 18:35:25 +0300

    feat: start TS course

---
+++ src/sandbox.ts
@@ -0,0 +1,61 @@
+// const character = "mario";
+// console.log(character);
+
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+
+// inputs.forEach((input) => console.log(input));
+
+// let age = '30';
+
+// const circ = (diameter: number) => {
+//     return diameter * Math.PI;
+// };
+
+// console.log(circ(500)); //it is checked now, during development, not at runtime
+
+// let names = ['luigi', 'mario', 'roshi'];
+// names.push('toad');
+// names.push(123); //does not work
+
+// let mixed = ['ken', 4, 'chic', 2, true];
+
+// mixed.push({}); //returns error!
+
+// let ninja = {
+//     name: 'mario',
+//     belt: 'black',
+//     age: 30,
+// };
+
+// ninja.age = 40;
+// ninja.isTrue = true; //cannot do that
+
+// ninja = {
+//     name: 'sad',
+//     belt: 'wr',
+//     age: '213', //error! should match the initial structure
+// };
+
+//explicit types
+
+// let character: string;
+// let age: number;
+// let isLogged: boolean;
+
+// let ninjas: string[] = [];
+
+// //union types
+
+// let mixed: (string | number)[] = [];
+
+// let uid: string | number; //normal variable
+
+// //objects
+// let ninja: {
+//     name: string;
+//     age: number;
+// };
+// let ninjaTwo: object; //can be an array!
+
+console.log('test');
